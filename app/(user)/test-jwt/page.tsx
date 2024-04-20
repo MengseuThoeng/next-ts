bd1ef89de1cc99af155bd21f31e0ac6eb3dbb527
MengseuThoeng <mengseu2004@gmail.com>
2024-04-20 13:46:23 +0700

    first commit

---
+++ app/(user)/test-jwt/page.tsx
@@ -0,0 +1,19 @@
+"use-client"
+import React, { useState } from 'react'
+
+export default function TestJWT() {
+    const [accessToken,SetAccessToken]=useState("")
+    const handleLogin= async()=>{
+        const email="mengseuslot3@gmail.com";
+        const password="Wtfbro1234"
+        //  
+    }
+  return (
+    <div>
+      <main className='h-screen grid place-content-center'>
+        JWT
+      </main>
+    </div>
+  )
+}
+
